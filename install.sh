6b13ef5ae5bec52bb23109d76af51b9e2066ad47
Alessandro Rosato <shelakynn@users.noreply.github.com>
2019-07-04 15:04:53 +0200

    Small script to move stuff around.

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# vim
+if [ -f $HOME/.vimrc ]; then
+	mv $HOME/.vimrc $HOME/.vimrc.backup
+fi
+
+cp ./my.vimrc $HOME/.vimrc
+
+if [ ! -d $HOME/.vim/swap ]; then
+  mkdir -p $HOME/.vim/swap;
+fi
+
+if [ ! -d $HOME/.vim/backup ]; then
+  mkdir -p $HOME/.vim/backup;
+fi
+
+# bash
+if [ -f $HOME/.bash_aliases ]; then
+    mv $HOME/.bash_aliases $HOME/.bash_aliases.backup
+fi
+
+cp ./my.bash_aliases $HOME/.bash_aliases
+
+if [ -f $HOME/.bash_profile ]; then
+    mv $HOME/.bash_profile $HOME/.bash_profile.backup
+fi
+
+cp ./my.bash_profile $HOME/.bash_profile
+
+if [ -f $HOME/.bashrc ]; then
+    mv $HOME/.bashrc $HOME/.bashrc.backup
+fi
+
+cp ./my.bashrc $HOME/.bashrc
